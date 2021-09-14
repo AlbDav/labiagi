f60195aa21ac63b0699a3fb3b6fde2286a160481
Davide Albano <davidealbano96@gmail.com>
2021-09-14 12:01:48 +0200

    changed to class form

--- src/scan.py
+++ src/scan.py
@@ -5,18 +5,29 @@ from sensor_msgs.msg import LaserScan
 import time
 import math
 
-def callback(msg):
-    print(msg)
-    for i, val in enumerate(msg.ranges, start=0):
-        angle = msg.angle_min + (i * msg.angle_increment)
-        x = val * math.cos(angle)
-        y = val * math.sin(angle)
-        print(x, y, angle, val)
-        if(val > 10):
-            continue
-        # print(i, val)
-    time.sleep(120)
+class obstacleDetection:
+    def __init__(self):
+        self.sub = rospy.Subscriber('/base_scan', LaserScan, self.callback)
 
-rospy.init_node('scan_values')
-sub = rospy.Subscriber('/base_scan', LaserScan, callback)
-rospy.spin()
+    def callback(self, msg):
+        print(msg)
+        for i, val in enumerate(msg.ranges, start=0):
+            angle = msg.angle_min + (i * msg.angle_increment)
+            x = val * math.cos(angle)
+            y = val * math.sin(angle)
+            print(x, y, angle, val)
+            if(val > 10):
+                continue
+            # print(i, val)
+        time.sleep(120)
+
+def main():
+    od = obstacleDetection()
+    rospy.init_node('obstacle_avoidance', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+if __name__ == '__main__':
+    main()
