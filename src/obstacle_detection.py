995f8972575839062587a008d3f55e87fa9a88f4
Davide Albano <davidealbano96@gmail.com>
2021-09-18 12:41:28 +0200

    started adding visualization

--- src/obstacle_detection.py
+++ src/obstacle_detection.py
@@ -5,13 +5,46 @@ from sensor_msgs.msg import LaserScan
 import time
 import math
 from collision_avoidance.srv import ForceResponse
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Pose
+from tf.transformations import quaternion_from_euler
 
 class obstacleDetection:
     def __init__(self):
         self.sub = rospy.Subscriber('/base_scan', LaserScan, self.callback)
-        force = ForceResponse(3,2)
-        print(force.magnitude)
-        print(force.angle)
+        self.pub = rospy.Publisher('marker', Marker, queue_size=10)
+        self.marker = Marker()
+        self.marker.header.frame_id = '/base_link'
+        self.marker.header.stamp = rospy.get_rostime()
+        self.marker.ns = 'robot'
+        self.marker.id = 0
+        self.marker.type = 0
+        self.marker.action = 0
+        self.marker.pose.position.x = 0
+        self.marker.pose.position.y = 0
+        self.marker.pose.position.z = 0
+        q = quaternion_from_euler(0, 0, 3.14)
+        self.marker.pose.orientation.x = q[0]
+        self.marker.pose.orientation.y = q[1]
+        self.marker.pose.orientation.z = q[2]
+        self.marker.pose.orientation.w = q[3]
+        self.marker.scale.x = 4
+        self.marker.scale.y = 0.05
+        self.marker.scale.z = 0.05
+
+        self.marker.color.r = 0.0
+        self.marker.color.g = 1.0
+        self.marker.color.b = 0.0
+        self.marker.color.a = 1.0
+
+        self.marker.lifetime = rospy.Duration(0)
+
+        while not rospy.is_shutdown():
+            self.pub.publish(self.marker)
+            time.sleep(1)
+        # force = ForceResponse(3,2)
+        # print(force.magnitude)
+        # print(force.angle)
 
     def callback(self, msg):
         # print(msg)
--- src/twist_generator.py
+++ src/twist_generator.py
@@ -16,11 +16,11 @@ class twistGenerator:
         vel_msg.angular.x = 0
         vel_msg.angular.y = 0
         vel_msg.angular.z = 0;
-        while not rospy.is_shutdown():
-            vel_msg.linear.x = random() * 100;
-            vel_msg.angular.z = -5 + random() * 10;
-            self.pub.publish(vel_msg)
-            sleep(1)
+        #while not rospy.is_shutdown():
+        #    vel_msg.linear.x = random() * 100;
+        #    vel_msg.angular.z = -5 + random() * 10;
+        #    self.pub.publish(vel_msg)
+        #    sleep(1)
 
 
 def main():
